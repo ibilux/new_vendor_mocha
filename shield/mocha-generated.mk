5e5fc67dcfde9936b58b872152101b77f712cc2e
Dargons10 <jdamr2p@gmail.com>
2019-11-01 15:13:18 +0100

    mocha: Nougat

---
+++ shield/mocha-generated.mk
@@ -0,0 +1,71 @@
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the License);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_PACKAGES += \
+                    libnvos \
+                    libnvddk_2d_v2 \
+                    libnvrm \
+                    libnvrm_graphics \
+                    libnvddk_vic \
+                    libnvgr \
+                    libnvmm_utils \
+                    libnvavp \
+                    libnvtvmr \
+                    libnvmm \
+                    libcuda \
+                    libphs \
+                    libnvrm_gpu \
+                    gralloc.tegra \
+                    libnvblit \
+                    hwcomposer.tegra \
+                    libnvhwc_service \
+                    libtlk_secure_hdcp_up \
+                    libtsechdcp \
+                    keystore.tegra \
+                    memtrack.tegra \
+                    vulkan.tegra \
+                    libmplmpu \
+                    libmllite \
+                    downloader \
+                    tlk_daemon \
+                    ss_status \
+                    rm_ts_server \
+                    librm_ts_service \
+                    ts.default \
+                    libEGL_tegra \
+                    libtsec_wrapper \
+                    libnvglsi \
+                    libnvrmapi_tegra \
+                    libnvcpl \
+                    libGLESv1_CM_tegra \
+                    libglcore \
+                    libGLESv2_tegra \
+                    libnvRSDriver \
+                    libnvRSCompiler \
+                    libnvwsi \
+                    libnvmm_audio \
+                    libnvmm_msaudio \
+                    libnvmm_parser \
+                    libnvmm_contentpipe \
+                    libnvmm_writer \
+                    libnvmmlite_audio \
+                    libnvmmlite_utils \
+                    libnvmmlite \
+                    libnvmmlite_image \
+                    libnvparser \
+                    libnvmmlite_video \
+                    libnvtnr \
+                    libnvomx \
+                    libnvomxadaptor \
+                    libnvomxilclient
